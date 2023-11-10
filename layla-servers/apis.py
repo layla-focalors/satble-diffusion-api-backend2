2e9d5c0a5a2bc64f07068aa5e0b3bab28e390c1f
arisia <20231331@daejin.ac.kr>
2023-11-10 10:49:28 +0900

    add manual api

--- layla-servers/apis.py
+++ layla-servers/apis.py
@@ -94,8 +94,72 @@ async def generate_info(uuids: str):
     print(uuids)
     return vector
 
-# uuid 기반 image searcer
-# example url : http://127.0.0.1:7865/generate/info/014fef84-19f5-4a0c-acbe-4dee49d9934e
+# # pro request ( ADD Model Selection )
+# http://127.0.0.1:7865/v1/generate/width=512&height=512&steps=50&seed=-1&cfgscale=7&batch_size=1$sea&water&beach&sketch$error&lowres
+# $ : options / prompts 구분
+# & : prompts 구분
+# = : options 구분
 
+# width    | # negative prompt
+# height    | 
+# steps
+# seed
+# cfgscale
+# prompt
+
+@app.get("/v1/generate/{prompt}")
+def generate(prompt: str, Request: fastapi.Request):
+    import uuid
+    prompts = prompt.split('$')
+    options = prompts[0].split('&')
+    prompt = prompts[1].split('&')
+    negative_prompt = prompts[2].split('&')
+    
+    width = options[0].split('=')[1]
+    height = options[1].split('=')[1]
+    steps = options[2].split('=')[1]
+    seed = options[3].split('=')[1]
+    cfgscale = options[4].split('=')[1]
+    batch_size = options[5].split('=')[1]
+    
+    prompts = ", ".join(prompt)
+    negative_prompt = ", ".join(negative_prompt)
+    
+    if int(width) >= 4096 | int(height) >= 8095:
+        return "Error : 가로 혹은 세로 길이가 너무 큽니다."
+    elif int(steps) >= 100:
+        return "Error : 스텝 수가 너무 높습니다. 스텝을 변경한 후 요청해주세요"
+    elif int(batch_size) >= 5:
+        return "Error : 배치 사이즈가 너무 큽니다. 잠시 후 다시 시도해주세요"
+    
+    payload = {
+        "prompt": prompts,
+        "negative_prompt": negative_prompt,
+        "seed": int(seed),
+        "batch_size": int(batch_size),
+        "steps": int(steps),
+        "cfg_scale": int(cfgscale),
+        "width": int(width),
+        "height": int(height)
+    }
+    
+    response = requests.post(url=f'{URL}/sdapi/v1/txt2img', json=payload)
+    r = response.json()
+    
+    for i in r['images']:
+        png_payload = { 
+            "image": "data:image/png;base64," + i
+        }
+    
+    uuids = uuid.uuid4()
+    os.chdir('C:\\Users\\user\\Desktop\\stable-diffusion-webui\\layla-servers\\cache')
+    image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
+    image.save(f'{uuids}.png')
+    response2 = requests.post(url=f'{URL}/sdapi/v1/png-info', json=png_payload)
+    
+    vector = FileResponse(f'{uuids}.png', media_type='image/png')
+    
+    return vector
+    
 if __name__ == "__main__":
     uvicorn.run("apis:app", port=7865, reload=True)
