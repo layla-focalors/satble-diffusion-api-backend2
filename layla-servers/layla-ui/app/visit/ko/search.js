1da0832b297d42c3f58a9a0df6ef7ea62f311510
arisia <20231331@daejin.ac.kr>
2023-11-15 18:44:42 +0900

    add next js ui shell

---
+++ layla-servers/layla-ui/app/visit/ko/search.js
@@ -0,0 +1,11 @@
+export default function SEC(){
+    return (
+        <div style={{display:"flex", alignItems:"center"}}>
+            <div className=".bg-transparent" style={{width:"180px", height:"30px", color:"#707070", borderRadius:"15px", marginLeft:"180px"}}>
+
+            </div>
+        </div>
+    )
+}
+
+export { SEC }
