568ca421163f159f075b597eb9dfe7e11eb38572
arisia <20231331@daejin.ac.kr>
2023-11-16 11:32:45 +0900

    add update

--- layla-servers/layla-ui/app/visit/ko/search.js
+++ layla-servers/layla-ui/app/visit/ko/search.js
@@ -1,8 +1,8 @@
 export default function SEC(){
     return (
         <div style={{display:"flex", alignItems:"center"}}>
-            <div className=".bg-transparent" style={{width:"180px", height:"30px", color:"#707070", borderRadius:"15px", marginLeft:"180px"}}>
-
+            <div className=".bg-transparent" style={{width:"180px", height:"30px", opacity:"40%", borderWidth:"1px", borderColor:"#707070", borderRadius:"15px", marginLeft:"180px"}}>
+                {/* INPUT 태그 넣고 준비 */}
             </div>
         </div>
     )
